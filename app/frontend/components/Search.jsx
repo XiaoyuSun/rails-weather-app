95228c83858e6968716583d77422490dd971152e
Xiaoyu Sun <sunxy311@gmail.com>
2024-03-28 09:36:17 -0700

    update seeds data

--- app/frontend/components/Search.jsx
+++ app/frontend/components/Search.jsx
@@ -2,55 +2,6 @@ import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
 import Loading from "./Loading";
 
-// const test = {
-//   weather_data: {
-//     coord: {
-//       lon: -122.2243,
-//       lat: 47.7918,
-//     },
-//     weather: [
-//       {
-//         id: 804,
-//         main: "Clouds",
-//         description: "overcast clouds",
-//         icon: "04d",
-//       },
-//     ],
-//     base: "stations",
-//     main: {
-//       temp: 283.29,
-//       feels_like: 282.71,
-//       temp_min: 281.38,
-//       temp_max: 285.16,
-//       pressure: 1005,
-//       humidity: 90,
-//     },
-//     visibility: 10000,
-//     wind: {
-//       speed: 6.17,
-//       deg: 140,
-//       gust: 9.77,
-//     },
-//     clouds: {
-//       all: 100,
-//     },
-//     dt: 1711576191,
-//     sys: {
-//       type: 2,
-//       id: 2036677,
-//       country: "US",
-//       sunrise: 1711547771,
-//       sunset: 1711593103,
-//     },
-//     timezone: -25200,
-//     id: 0,
-//     name: "Bothell",
-//     cod: 200,
-//   },
-//   cached: false,
-//   error_message: null,
-// };
-
 const isValidZipCode = (zipCode) => {
   return /^(\d{5})$|^(\d{5}-\d{4})$/.test(zipCode);
 };
@@ -109,7 +60,6 @@ const Search = () => {
       {error_message && <p className="text-red-500">{error_message}</p>}
       {loading && <Loading />}
       {forecasts && <WeatherInfo forecasts={forecasts} />}
-      {/* {test && <WeatherInfo forecasts={test} />} */}
     </div>
   );
 };
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,50 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-json = {
-  "name": "Bothell",
-  "latitude": 47.7735,
-  "longitude": -122.204,
-  "country": "US",
-  "population": 47415,
-  "is_capital": false
-}
 
-Forecast.create!([{ target: 'bothell', weather_data: JSON.generate(json) }])
+  weather_data = {
+    coord: {
+      lon: -122.2243,
+      lat: 47.7918,
+    },
+    weather: [
+      {
+        id: 804,
+        main: "Clouds",
+        description: "overcast clouds",
+        icon: "04d",
+      },
+    ],
+    base: "stations",
+    main: {
+      temp: 283.29,
+      feels_like: 282.71,
+      temp_min: 281.38,
+      temp_max: 285.16,
+      pressure: 1005,
+      humidity: 90,
+    },
+    visibility: 10000,
+    wind: {
+      speed: 6.17,
+      deg: 140,
+      gust: 9.77,
+    },
+    clouds: {
+      all: 100,
+    },
+    dt: 1711576191,
+    sys: {
+      type: 2,
+      id: 2036677,
+      country: "US",
+      sunrise: 1711547771,
+      sunset: 1711593103,
+    },
+    timezone: -25200,
+    id: 0,
+    name: "Bothell",
+    cod: 200,
+  },
+
+Forecast.create!([{ target: '98021', weather_data: JSON.generate(weather_data) }])
